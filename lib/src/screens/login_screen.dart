2d5734f02634cca41be9f12d3a3dbbadcc7bda53
webdevelopua <js.develop.com@gmail.com>
2021-03-25 03:55:34 +0200

    Add LoginScreen stateless widget

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -1,70 +1,44 @@
 import 'package:flutter/material.dart';
-import '../mixins/validation_mixin.dart';
 
-class LoginScreen extends StatefulWidget {
-  createState() {
-    return LoginScreenState();
-  }
-}
-
-class LoginScreenState extends State<LoginScreen> with ValidationMixin {
-  final formKey = GlobalKey<FormState>();
-  String email = '';
-  String password = '';
-
+class LoginScreen extends StatelessWidget {
   Widget build(context) {
     return Container(
-      child: Form(
-        key: formKey,
-        child: Column(
-          children: [
-            emailField(),
-            passwordField(),
-            Container(margin: EdgeInsets.only(top: 25.0)),
-            submitButton(),
-          ],
-        ),
-      ),
       margin: EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          emailField(),
+          passwordField(),
+          Container(margin: EdgeInsets.only(top: 25.0)),
+          submitButton(),
+        ],
+      ),
     );
   }
 
   Widget emailField() {
-    return TextFormField(
+    return TextField(
       keyboardType: TextInputType.emailAddress,
       decoration: InputDecoration(
         labelText: 'Email',
         hintText: 'name@domain.com',
       ),
-      validator: validateEmail,
-      onSaved: (String value) {
-        email = value;
-      },
     );
   }
 
   Widget passwordField() {
-    return TextFormField(
+    return TextField(
       obscureText: true,
       decoration: InputDecoration(
         labelText: 'Password',
         hintText: '********',
       ),
-      validator: validatePassword,
-      onSaved: (String value) {
-        password = value;
-      },
     );
   }
 
   Widget submitButton() {
     return ElevatedButton(
       onPressed: () {
-        if (formKey.currentState.validate()) {
-          formKey.currentState.save();
-          print('Credentials: $email & $password');
-        }
-        return print('Validation Status: ${formKey.currentState.validate()}');
+        return print('Submitted');
       },
       child: Text('Submit'),
       style: ButtonStyle(
